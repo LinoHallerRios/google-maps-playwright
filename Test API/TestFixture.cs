9694fd06c4a2bb12658d08db7711e90ab2c17488
Lino Haller <linohr00@gmail.com>
2024-05-15 20:15:52 +0200

    test: run tests on all browsers

--- Test API/TestFixture.cs
+++ Test API/TestFixture.cs
@@ -3,48 +3,22 @@ using static GoogleMapsPlaywright.Test_API.Locations;
 
 namespace GoogleMapsPlaywright.Test_API;
 
-[Parallelizable(ParallelScope.Self)]
-public class TestFixture : PlaywrightTest
+[Parallelizable(ParallelScope.Children)]
+public class TestFixture : BrowserTest
 {
     string GoogleMaps => "https://www.google.com/maps/";
     
-    IBrowserContext context;
-    IBrowser browser;
-
-    [SetUp]
-    public async Task Setup()
-    {
-        browser = await LaunchChromium();
-
-        await CreateBrowserFrom(Berlin);
-    }
-
-    async Task<IBrowser> LaunchChromium()
-    {
-        return await Playwright.Chromium.LaunchAsync(new()
-        {
-            Headless = false,
-            Timeout = 3000,
-        });
-    }
-
-    async Task CreateBrowserFrom(Geolocation where)
-    {
-        context = await browser.NewContextAsync(
-            new BrowserNewContextOptions()
-            {
-                ViewportSize = new ViewportSize() { Width = 1280, Height = 720 },
-                Locale = "en-UK",
-                TimezoneId = "Europe/Berlin",
-                Permissions = new[] { "geolocation" },
-                Geolocation = where,
-                IgnoreHTTPSErrors = true
-            });
-    }
-
     protected async Task<IPage> LoadGoogleMapsPage()
     {
-        var Map = await context.NewPageAsync();
+        var Map = await Browser.NewPageAsync(new BrowserNewPageOptions()
+        {
+            ViewportSize = new ViewportSize() { Width = 1280, Height = 720 },
+            Locale = "en-UK",
+            TimezoneId = "Europe/Berlin",
+            Permissions = new[] { "geolocation" },
+            Geolocation = Berlin,
+            IgnoreHTTPSErrors = true
+        });
 
         await Map.GotoAsync(GoogleMaps);
 
